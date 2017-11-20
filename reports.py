9744dba8c6f12939eb18fbda988d08404a3c836e
gabor <roskogabor@gmail.com>
2017-11-20 17:47:02 +0100

    get_latest function is passed

--- reports.py
+++ reports.py
@@ -1,10 +1,10 @@
-
 # Report functions
 def count_games(file_name):
     with open(file_name, "r") as gamedata:
         games = gamedata.readlines()
         number = len(games)
     return number
+
 
 def decide(file_name, year):
     yearlist = []
@@ -18,5 +18,27 @@ def decide(file_name, year):
         result = True if int(year) in yearlist else False
     return result
 
+
+def get_latest(file_name):
+    latest = ""
+    with open(file_name, "r") as yeardata:
+        games = yeardata.readlines()
+        datas = []
+        for i in range(len(games)):
+            datalist = games[i].split("\t")
+            datas.append(datalist)
+        for i in range(len(datas)):
+            if latest == "" or datas[i][2] > latest[1]:
+                latest = [datas[i][0],datas[i][2]]
+        return latest[0]
+
+
+def count_by_genre(file_name, genre):
+    pass
+
+
+def get_line_number_by_title(file_name, title):
+    pass
+
 if __name__ == "__main__":
-    print(decide("game_stat.txt",2000))
+    print(get_latest("game_stat.txt"))
