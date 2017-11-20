e4623936a8661c96e04247bdda63e952d17f484b
gabor <roskogabor@gmail.com>
2017-11-20 16:38:32 +0100

    decide function done tested working passed

--- reports.py
+++ reports.py
@@ -6,5 +6,17 @@ def count_games(file_name):
         number = len(games)
     return number
 
+def decide(file_name, year):
+    yearlist = []
+    result = False
+    with open(file_name, "r") as yeardata:
+        games = yeardata.readlines()
+        for i in range(len(games)):
+            data = games[i].split("\t")
+            gameyear = int(data[2])
+            yearlist.append(gameyear)
+        result = True if int(year) in yearlist else False
+    return result
+
 if __name__ == "__main__":
-    print(count_games("game_stat.txt"))
+    print(decide("game_stat.txt",2000))
