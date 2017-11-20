810772316820ecbbc2c22aa6236d4d8bceff12c8
gabor <roskogabor@gmail.com>
2017-11-20 15:06:12 +0100

    reports.py is tested with test.py and it said it's passed

--- reports.py
+++ reports.py
@@ -1,2 +1,10 @@
 
 # Report functions
+def count_games(file_name):
+    with open(file_name, "r") as gamedata:
+        games = gamedata.readlines()
+        number = len(games)
+    return number
+
+if __name__ == "__main__":
+    print(count_games("game_stat.txt"))
