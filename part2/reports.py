d372c5bf203b1c65402c827b1ea811783d6de577
gabor <roskogabor@gmail.com>
2017-11-21 12:26:26 +0100

    get_most_played done tested passed

--- part2/reports.py
+++ part2/reports.py
@@ -1,2 +1,17 @@
 
 # Report functions
+def get_most_played(file_name):
+    with open(file_name, "r") as gamedata:
+        games = gamedata.readlines()
+        datas = []
+        for i in range(len(games)):
+            datalist = games[i].split("\t")
+            datas.append(datalist)
+        most = ""
+        for i in range(len(datas)):
+            if most == "" or float(datas[i][1]) > most[1]:
+                most = [datas[i][0], float(datas[i][1])]
+    return most[0]
+
+if __name__ == "__main__":
+    print(get_most_played("game_stat.txt"))
