573949fb115ce7de30cbbfa4933818c57a14d1a2
gabor <roskogabor@gmail.com>
2017-11-22 10:26:58 +0100

    i was wrong i wrote the test2 export file so i moved the code in this dirs export.py file

--- export.py
+++ export.py
@@ -1,2 +1,19 @@
+import printing
+
+
+file_name = "game_stat.txt"
+gameyear = "2004"
+gametitle = "Diablo II"
+gamegenre = "First-person shooter"
+
 
 # Export functions
+def exportdata(datas):
+    with open("export.txt", "w") as data:
+        for i in range(len(datas)):
+            data.write(str(datas[i]) + "\n")
+
+
+if __name__ == "__main__":
+    gamedata = printing.print_datas(file_name, gameyear, gametitle, gamegenre)
+    exportdata(gamedata)
