573d418f779e580b2558e54aaa7f3dd725e8a334
gabor <roskogabor@gmail.com>
2017-11-22 11:44:26 +0100

    made new makelist function

--- reports.py
+++ reports.py
@@ -1,62 +1,61 @@
 # Report functions
+def openfile(fname):
+    with open(fname, "r") as datas:
+        datalist = datas.readlines()
+    return datalist
+
+
+def makelist(datas, indexof, datatype):
+    datalist = []
+    for i in range(len(datas)):
+        data = datas[i].split("\t")
+        if indexof != "x":
+            datalist.append(datatype(data[indexof]))
+        else:
+            datalist.append(data)
+    return datalist
+
+
 def count_games(file_name):
-    with open(file_name, "r") as gamedata:
-        games = gamedata.readlines()
-        number = len(games)
+    games = openfile(file_name)
+    number = len(games)
     return number
 
 
 def decide(file_name, year):
-    yearlist = []
     result = False
-    with open(file_name, "r") as yeardata:
-        games = yeardata.readlines()
-        for i in range(len(games)):
-            data = games[i].split("\t")
-            gameyear = int(data[2])
-            yearlist.append(gameyear)
-        result = True if int(year) in yearlist else False
+    games = openfile(file_name)
+    yearlist = makelist(games, 2, int)
+    result = True if int(year) in yearlist else False
     return result
 
 
 def get_latest(file_name):
     latest = ""
-    with open(file_name, "r") as yeardata:
-        games = yeardata.readlines()
-        datas = []
-        for i in range(len(games)):
-            datalist = games[i].split("\t")
-            datas.append(datalist)
-        for i in range(len(datas)):
-            if latest == "" or datas[i][2] > latest[1]:
-                latest = [datas[i][0], datas[i][2]]
-        return latest[0]
+    games = openfile(file_name)
+    datas = makelist(games, "x", str)
+    for i in range(len(datas)):
+        if latest == "" or datas[i][2] > latest[1]:
+            latest = [datas[i][0], datas[i][2]]
+    return latest[0]
 
 
 def count_by_genre(file_name, genre):
-    with open(file_name, "r") as genredata:
-        genres = genredata.readlines()
-        datas = []
-        for i in range(len(genres)):
-            datalist = genres[i].split("\t")
-            datas.append(datalist)
-        counter = 0
-        for i in range(len(datas)):
-            if datas[i][3] == genre:
-                counter += 1
-        return counter
+    genres = openfile(file_name)
+    datas = makelist(genres, "x", str)
+    counter = 0
+    for i in range(len(datas)):
+        if datas[i][3] == genre:
+            counter += 1
+    return counter
 
 
 def get_line_number_by_title(file_name, title):
-    with open(file_name, "r") as titledata:
-        datas = titledata.readlines()
-        titles = []
-        for i in range(len(datas)):
-            titlelist = datas[i].split("\t")
-            titles.append(titlelist[0])
-        listindex = titles.index(title)
+    datas = openfile(file_name)
+    titles = makelist(datas, 0, str)
+    listindex = titles.index(title)
     return int(listindex) + 1
 
 
 if __name__ == "__main__":
-    print(get_line_number_by_title("game_stat.txt", "Diablo III"))
+    print(get_line_number_by_title("game_stat.txt", "Counter-Strike"))
