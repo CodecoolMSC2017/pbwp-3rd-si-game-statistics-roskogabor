89c20c627391b7374c520d7b713b9d5bcab7424b
gabor <roskogabor@gmail.com>
2017-11-22 10:03:53 +0100

    tested code with printing.py and test datas

--- printing.py
+++ printing.py
@@ -1,2 +1,10 @@
+import reports
 
+
+file_name = "game_stat.txt"
 # Printing functions
+print(reports.count_games(file_name))
+print(reports.decide(file_name, "2004"))
+print(reports.get_latest(file_name))
+print(reports.count_by_genre(file_name, "First-person shooter"))
+print(reports.get_line_number_by_title(file_name, "Diablo II"))
