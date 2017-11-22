d8182c97328f87b763fd9c1b4faf15583d242b18
gabor <roskogabor@gmail.com>
2017-11-22 10:14:04 +0100

    modified printing.py by pep8 and made a function into it to print datas

--- printing.py
+++ printing.py
@@ -2,9 +2,22 @@ import reports
 
 
 file_name = "game_stat.txt"
+gameyear = "2004"
+gametitle = "Diablo II"
+gamegenre = "First-person shooter"
+
+
 # Printing functions
-print(reports.count_games(file_name))
-print(reports.decide(file_name, "2004"))
-print(reports.get_latest(file_name))
-print(reports.count_by_genre(file_name, "First-person shooter"))
-print(reports.get_line_number_by_title(file_name, "Diablo II"))
+def print_datas(fname, year, title, genre):
+    countgames = reports.count_games(fname)
+    decideres = reports.decide(fname, year)
+    getlatest = reports.get_latest(fname)
+    cbygenre = reports.count_by_genre(fname, genre)
+    getlnumbytit = reports.get_line_number_by_title(fname, title)
+    return [countgames, decideres, getlatest, cbygenre, getlnumbytit]
+
+
+if __name__ == "__main__":
+    datas = print_datas(file_name, gameyear, gametitle, gamegenre)
+    for i in range(len(datas)):
+        print(datas[i])
